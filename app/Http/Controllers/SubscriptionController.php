f7ce1130a1943edddf757cd97ba858d0a61be306
serugo <serugojanvier@gmail.com>
2023-04-01 14:07:09 +0200

    correct errors in subscription controller

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Subscription;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Validator;
+use Illuminate\Support\Facades\Validator;
 
 class SubscriptionController extends Controller
 {
